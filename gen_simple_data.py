4fca92a26f5573be80f092df6ccfbeb5af7d7cbd
Watchara <51770964+wcr2000@users.noreply.github.com>
2024-05-02 11:41:44 +0700

    update fucntion create table

--- gen_simple_data.py
+++ gen_simple_data.py
@@ -3,6 +3,41 @@ import psycopg2
 import connect_postgres_db as cn
 
 
+def create_table(connection):
+    if connection is not None:
+        try:
+            # Create a cursor object using the connection
+            cursor = connection.cursor()
+
+            # SQL statement to create a table
+            create_table_query = """CREATE TABLE IF NOT EXISTS your_table_name (
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR(50),
+                last_name VARCHAR(50),
+                age INTEGER
+            )"""
+
+            # Execute the create table query
+            cursor.execute(create_table_query)
+
+            # Commit the transaction
+            connection.commit()
+
+            print("Table created successfully!")
+
+            # Close the cursor
+            cursor.close()
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error while creating table in PostgreSQL:", error)
+
+        finally:
+            # Close the connection
+            connection.close()
+    else:
+        print("No connection to the database.")
+
+
 def insert_data(connection):
     if connection is not None:
         try:
@@ -39,6 +74,9 @@ def insert_data(connection):
 if __name__ == "__main__":
     # Connect to the database
     connection = cn.connect_to_db()
+
+    # Create table
+    create_table(connection)
 
     # Insert data
     insert_data(connection)
